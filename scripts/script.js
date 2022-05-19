2ec27a10fa499e9cae30e32a19bf45b6fb0ef9a9
Dinesh Kumar <dkumar@epostglobalshipping.com>
2022-05-18 20:04:31 -0400

    Seventh Commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
 if('serviceWorker' in navigator)
 {
-    navigator.serviceWorker.register('../service-worker.js',{scope: '/week2/'});
+    navigator.serviceWorker.register('../service-worker.js',{scope: '/week2/'}).then(function()
+    {
+        console.log('sw - ok');
+    });
 }
